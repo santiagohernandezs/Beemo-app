0e9088bc601e66228e1cd6c82ffbbc78df261bb2
Santiago Hernandez <santiagooheernandez@gmail.com>
2022-06-07 15:44:07 -0400

    first commit

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, '../node_modules', '.bin', 'electron')
+})
+
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
